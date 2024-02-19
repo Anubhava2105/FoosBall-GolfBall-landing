21bc16a11651762cb6ec8a775a544a13dc1ae1a6
Anubhava2105 <118335179+Anubhava2105@users.noreply.github.com>
2024-02-19 22:30:35 +0530

    rules_cards+ javascript+media_queies
    added the remaining html for the cards, also added some media queries to make it responsive
    added javascript to change the text when foosball or golfball is clicked.

---
+++ Anubhav/app.js
@@ -0,0 +1,16 @@
+function change_text() {
+    var p_1 = document.getElementById("p-1");
+    var p_2 = document.getElementById("p-2");
+    var p_3 = document.getElementById("p-3");
+    p_1.innerHTML ="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime illo quidem amet neque, nisi vel. Quas magnam veritatis suscipit illum, amet asperiores rem aperiam, quos velit iusto, accusantium quibusdam ab";
+    p_2.innerHTML ="Animi rerum, veniam unde aut fugiat temporibus eum nemo suscipit ducimus nihil ipsum magni iusto autem corporis voluptatibus ex, dignissimos veritatis. Lorem ipsum.";
+    p_3.innerHTML ="quam perferendis quibusdam eius quis, placeat laudantium aut id, cupiditate fuga debitis modi consequatur error mollitia vitae veniam ducimus? Magni voluptatum mollitia labore veniam.";
+}
+function change_back() {
+    var p_1 = document.getElementById("p-1");
+    var p_2 = document.getElementById("p-2");
+    var p_3 = document.getElementById("p-3");
+    p_1.innerHTML ="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque blanditiis, cupiditate, cum tenetur error eum expedita facere perspiciatis quia ut et eius rem quae assumenda quos dolorum consequuntur qui soluta.Labore, molestiae non. Nam voluptas quidem saepe quia aperiam dolorem iure adipisci provident perspiciatis ducimus, consequuntur sed facere in dolores officia dolore ut quas numquam necessitatibus exercitationem voluptatum odit alias.";
+    p_2.innerHTML ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nam laudantium quidem exercitationem error, maxime, beatae ea reprehenderit in, molestias ipsa. Ex, harum reprehenderit! Esse perferendis harum ratione. Quasi, libero? Qui necessitatibus, vel magnam itaque animi nihil. Animi rerum, veniam unde aut fugiat temporibus eum nemo suscipit ducimus nihil ipsum magni iusto autem corporis voluptatibus ex, dignissimos veritatis";
+    p_3.innerHTML="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo obcaecati perspiciatis in nesciunt officiis ipsa itaque. Odit, maiores, ad eaque minus blanditiis in labore aut explicabo dicta quam, harum iusto. Possimus pariatur magni quam perferendis quibusdam eius quis, placeat laudantium aut id, cupiditate fuga debitis modi consequatur error mollitia vitae veniam ducimus? Magni voluptatum mollitia labore veniam.";
+}
